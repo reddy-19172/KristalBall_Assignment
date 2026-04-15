ebcc01b92511dd480e6a18df4542e0c08a6d87b0
Nithin <nithinreddyvari72@gmail.com>
2026-04-15 22:47:04 +0530

    Company_Project

--- backend/index.js
+++ backend/index.js
@@ -167,6 +167,8 @@ app.post('/api/assignments', auth(['admin', 'commander']), (req, res) => {
 //
 // 🚀 START SERVER
 //
-app.listen(5000, () => {
-  console.log('Server running on port 5000')
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
